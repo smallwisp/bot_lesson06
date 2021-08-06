03fef679ce47ec9887eb27241f0ab54b5aade560
smallwisp <4ubarov96@mail.ru>
2021-08-06 21:44:10 +0300

    completed tasks of lesson 6

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function guessNumber() {
+   let n = 89;
+
+   function isNumber(n) {
+      return !isNaN(parseFloat(n)) && isFinite(n);
+   }
+   
+   let guessing = prompt('Угадай число от 1 до 100');
+   if (guessing === null) {
+      alert('Игра окончена');
+   } else if (!isNumber(guessing)) {
+      alert('Введи число!');
+      guessNumber(n);
+   } else if (+guessing === n) {
+      alert('Поздравляю, Вы угадали!');
+   } else if (+guessing > n) {
+      alert('Загаданное число меньше');
+      guessNumber(n);
+   } else {
+      alert('Загаданное число больше');
+      guessNumber(n);
+   } 
+
+}
+
+guessNumber();
+
