d667858862560febe4c94d15eeaa39bf73c4dc7f
smallwisp <4ubarov96@mail.ru>
2021-08-09 16:36:22 +0300

    added closure gameConditions() to let n

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict'
 
-function getRandomNumber() {
+let randomNumber = function() {
    return Math.round(Math.random() * 100);
 }
 
+let res = randomNumber();
+
 function guessNumber(attempts) {
    
-   let n = getRandomNumber();
+   let n = res;
 
    console.log(n);
 
@@ -14,35 +16,39 @@ function guessNumber(attempts) {
       return !isNaN(parseFloat(guessing)) && isFinite(guessing);
    }
    
-   let guessing = prompt('Угадай число от 1 до 100');
-   if (guessing === null) {
-      alert('Игра окончена');
-   } else if (!isNumber(guessing)) {
-      alert('Введи число!');
-      guessNumber(attempts);
-   } else if (+guessing === n) {
-      let victory = confirm('Поздравляю, Вы угадали!Хотели бы сыграть еще?');
-      if (victory == true) {
-         guessNumber(10);
+   function gameConditions() {
+      let guessing = prompt('Угадай число от 1 до 100');
+      if (guessing === null) {
+         alert('Игра окончена');
+      } else if (!isNumber(guessing)) {
+         alert('Введи число!');
+         guessNumber(attempts);
+      } else if (+guessing === n) {
+         let victory = confirm('Поздравляю, Вы угадали!Хотели бы сыграть еще?');
+         if (victory == true) {
+            guessNumber(10);
+         } else {
+            alert('До встречи!')
+         }
+      } else if (attempts == 0) {
+         let defeat = confirm('Попытки закончились, хотите сыграть еще?');
+         if (defeat == true) {
+            guessNumber(10);
+         } else {
+            alert('До встречи!')
+         }
+      } else if (+guessing > n) {
+         alert(`Загаданное число меньше, осталось попыток : ${attempts}`);
+         guessNumber(attempts - 1);
+         console.log(attempts);
       } else {
-         alert('До встречи!')
-      }
-   } else if (attempts == 0) {
-      let defeat = confirm('Попытки закончились, хотите сыграть еще?');
-      if (defeat == true) {
-         guessNumber(10);
-      } else {
-         alert('До встречи!')
-      }
-   } else if (+guessing > n) {
-      alert(`Загаданное число меньше, осталось попыток : ${attempts}`);
-      guessNumber(attempts - 1);
-      console.log(attempts);
-   } else {
-      alert(`Загаданное число больше, осталось попыток : ${attempts}`);
-      guessNumber(attempts - 1);
-   } 
+         alert(`Загаданное число больше, осталось попыток : ${attempts}`);
+         guessNumber(attempts - 1);
+      } 
+      
+   }
 
+   return gameConditions();
 }
 
 guessNumber(10);
